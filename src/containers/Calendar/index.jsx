91540d54d9a6ff3ed3dd90b35578ce5b67298d7f
BEGEMOT9I <daston2012@gmail.com>
2017-06-05 10:41:02 +0300

    fixed calendar navigation

--- src/containers/Calendar/index.jsx
+++ src/containers/Calendar/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
+import moment from 'moment'
 
 import CalendarTooltip from '../../components/CalendarTooltip'
 import Categories from '../../components/Categories'
@@ -19,6 +20,10 @@ class Calendar extends Component {
     const now = new Date()
     if (now.getTime() < this.firstDay.getTime()) {
       this.firstDay = now
+    }
+
+    if (now.getTime() > this.lastDay.getTime()) {
+      this.lastDay = now
     }
   }
 
@@ -55,7 +60,11 @@ class Calendar extends Component {
     const { dispatch } = this.props
     let next_date = new Date(this.props.selectedYear + delta, this.props.selectedMonth)
 
-    if (next_date.getFullYear() < this.firstDay.getFullYear() || next_date.getFullYear() > this.lastDay.getFullYear()) {
+    if (
+      moment(next_date).isBefore(this.firstDay) &&
+      (next_date.getFullYear() < this.firstDay.getFullYear() || next_date.getMonth() < this.firstDay.getMonth()) ||
+      moment(next_date).isAfter(this.lastDay) &&
+      (next_date.getFullYear() > this.lastDay.getFullYear() || next_date.getMonth() > this.lastDay.getMonth())) {
       return
     }
 
@@ -74,7 +83,11 @@ class Calendar extends Component {
     const { dispatch } = this.props
     let next_date = new Date(this.props.selectedYear, this.props.selectedMonth + delta)
 
-    if (next_date.getMonth() < this.firstDay.getMonth() && next_date.getFullYear() <= this.firstDay.getFullYear() || next_date.getMonth() > this.lastDay.getMonth() && next_date.getFullYear() >= this.lastDay.getFullYear()) {
+    if (
+      moment(next_date).isBefore(this.firstDay) &&
+      (next_date.getFullYear() < this.firstDay.getFullYear() || next_date.getMonth() < this.firstDay.getMonth()) ||
+      moment(next_date).isAfter(this.lastDay) &&
+      (next_date.getFullYear() > this.lastDay.getFullYear() || next_date.getMonth() > this.lastDay.getMonth())) {
       return
     }
     
@@ -104,7 +117,7 @@ class Calendar extends Component {
           <svg className="nav__arrow nav__arrow_left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 477.175 477.175" onClick={ this.changeMonth.bind(this, -1) }>
             <path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/>
           </svg>
-          <span className="nav__title">{ this.props.selectedMonth } { this.props.selectedYear }</span>
+          <span className="nav__title">{ this.props.selectedMonth + 1 } { this.props.selectedYear }</span>
           <svg className="nav__arrow nav__arrow_right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 477.175 477.175" onClick={ this.changeMonth.bind(this, 1) }>
             <path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5 c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"/>
           </svg>
