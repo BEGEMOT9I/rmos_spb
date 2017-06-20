e429e55f6e77f2346770c7a1d9d307e7dc4f6e84
BEGEMOT9I <daston2012@gmail.com>
2017-06-20 21:18:10 +0300

    fixed router, added 404

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Branch.all.map { |branch| get branch.path => 'layout#index' }
-
-  get '/' => redirect(Branch.find(Rails.application.config.DEFAULT_BRANCH).path)
   
   get '/data' => 'data#index'
-  root 'layout#index'
+  get '/:url' => 'layout#index'
 end
