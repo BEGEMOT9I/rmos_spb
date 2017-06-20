0db362d1a86d8d29c1e5b0c52e5b604723856c95
BEGEMOT9I <daston2012@gmail.com>
2017-06-20 21:47:03 +0300

    fixed router

--- app/controllers/layout_controller.rb
+++ app/controllers/layout_controller.rb
@@ -2,6 +2,10 @@ class LayoutController < ApplicationController
   layout 'layout'
 
   def index
+    # if Branch.where(path: request.fullpath).count
+    #   render 'layouts/layout'
+    # else
+    #   render 'errors/404', layout: false, status: 404
     render 'layouts/layout'
   end
 end
